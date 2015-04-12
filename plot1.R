db18f0baa8aa11111f681ea5ca34361297d13a12
jiaminglee <leejia_ming@yahoo.com.sg>
2015-04-12 23:58:14 +0800

    Upload

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# Read file
+data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+# Set dates
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+# Read data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+# Convert dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+# Draw plot
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
